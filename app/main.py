20df736b314072ce0691aafe53a71d5932984885
md598 <Mark@duttonracing.com>
2020-09-04 19:19:32 -0500

    Updated with descriptions

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ import aiofiles
 import json
 
 from typing import List
-from pydantic import BaseModel,StrictFloat, validator, ValidationError
+from pydantic import BaseModel,StrictFloat, validator, ValidationError, Field
 from datetime import date, datetime, time, timedelta, tzinfo
 import sys
 import pandas as pd
@@ -18,22 +18,42 @@ import joblib
 import xgboost as xgb
 print(xgb.__version__)
 
+
+tags_metadata = [
+    {
+        "name":"predict_xg",
+        "description":"Predicted days until carrier possession. Will return 0 for same day, 1 for next day, 2 for two day, etc.",
+    },
+
+]
 version = f"{sys.version_info.major}.{sys.version_info.minor}"
 #path = Path(__file__).parent
-app = FastAPI()
+app = FastAPI(openapi_tags=tags_metadata)
+
+@app.on_event("startup")
+def startup_event():
+    #Need to redo with xgboost save... version control issues accross OSs
+    xgb_open = open("app/models/XGBoost_model_001.joblib.dat","rb") #docker version
+    #xgb_open = open("models/XGBoost_model_001.joblib.dat","rb") #local uvicorn
+    global xgb_model
+    xgb_model = joblib.load(xgb_open)
 
 #Will enforce the correct data types getting to the model
-class Data(BaseModel):
-    input_order_time:datetime
-    input_hours_truck:float
+class Order(BaseModel):
+    input_order_time:datetime = Field(...,title='Time of order creation',alias='order_datetime',
+    description='Time the order is placed. Must be a full datetime in the same timezone as the truck departure'\
+    'must use ISO 8601 format - see https://www.w3.org/TR/NOTE-datetime',
+    example='2020-07-31T23:43:27')
+
+    input_hours_truck:float = Field(...,gt=0,lt=120,title='Time until the next truck departs',alias='hours_to_truck',
+    description='Timedelta from when the order is placed until the trucks leave the fullfilment center, in hours.'\
+    'i.e. the order is placed on 10pm Saturday and the next truck leaves at 530PM on Monday,'\
+    ' then provide 43.5 hours. Use leading 0 if <1 )i.e. 0.5)',
+    example='8.25')
 
     @validator('input_order_time', pre=True, always=True)
     def set_ts_now(cls, v):
         return v or datetime.now()
-
-#Need to redo with xgboost save... version control issues accross OSs  
-xgb_open = open("app/models/XGBoost_model_001.joblib.dat","rb")
-xgb_model = joblib.load(xgb_open)
 
 
 @app.get("/")
@@ -42,31 +62,6 @@ def read_root():
     return {"message": message}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
-
-@app.post("/predict/")
-async def predict(data:Data):
-    order_time = data.input_order_time
-
-    truck_hours = data.input_hours_truck
-
-
-    return {"orig_value":order_time, "prediction":truck_hours-1}
-
-@app.post("/predict2/")
-async def predict2(data:List[Data]):
-    response_body = []
-    for item in data:
-        order_time = item.input_order_time
-        truck_hours = item.input_hours_truck
-
-
-        response_body.append({"Input_Time":item.input_order_time,"Input_Truck":item.input_hours_truck,
-        "Prediction":truck_hours-1,"Day":order_time.day,"Hour":order_time.hour,"Month":order_time.month,
-        "Year":order_time.year})
-    return response_body
 
 def add_dateparts(df):
     try:
@@ -83,34 +78,9 @@ def add_dateparts(df):
     df['Hour']=df['order_time'].dt.hour
     df['Elapsed']=df['order_time'].astype(np.int64) // 10 ** 9
 
-@app.post("/predict3/")
-async def predict3(data:List[Data]):
-    response_body = []
-    rows_list=[]
-    for item in data:
-        order_time = item.input_order_time
-        truck_hours = item.input_hours_truck
-        if(order_time.hour < 15):
-            is_before_3pm = True
-            response_body="true"
-        else:
-            is_before_3pm = False
-            response_body="false"
-        rows_list.append({'order_time':order_time,'time_to_next_truck_bis_hours':truck_hours,'is_before_3pm':is_before_3pm})
-    df=pd.DataFrame(rows_list)
-    add_dateparts(df)
 
-    df.head()
-    df.to_csv('results.csv',index=False)
-    df['order_time']=df['order_time'].dt.strftime('%Y-%m-%d-%H-%M-%S-%f-%Z')
-    response_json= df.to_json(orient="records",date_format='iso')
-    response_json= json.loads(response_json)
-    #json.dumps(response_json,indent=4)
-    #response_body= df.info()
-    return response_json
-
-@app.post("/predict4/")
-async def predict4(data:List[Data]):
+@app.post("/predict_xg/", tags=["predict_xg"])
+async def Predict_Days_To_Possession(data:List[Order]):
     rows_list=[]
     for item in data:
         order_time = item.input_order_time
@@ -131,7 +101,7 @@ async def predict4(data:List[Data]):
     response_json= json.loads(response_json)
     #json.dumps(response_json,indent=4)
     response_body= df.info()
-    
+
     #XGBoost predictions
     xgb_preds = xgb_model.predict_proba(df.drop(['order_time','Year'],axis=1))
     argmax = xgb_preds.argmax(axis=1)
@@ -139,10 +109,10 @@ async def predict4(data:List[Data]):
     response_body=predict
     lists = predict.tolist()
     json_str = json.dumps(lists)
-    
-    
+
+
     #nn_preds = learn.get_preds()[0]
-    
+
     print (xgb_preds)
     print(predict)
     print("XGB Version:")
